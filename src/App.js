a3a3dd40885f846cd091d34cfa957ac2b88b05e1
Lalit Bhagat <bhagatlalit46@gmail.com>
2022-12-01 23:11:27 +0530

    added fireabse

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Login from "./components/Login";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Details from "./components/Details";
+function App() {
+  return (
+    <div className="app">
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/detail/:id">
+            <Details />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/app/store.js
@@ -0,0 +1,10 @@
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import userReducer from "../features/user/userSlice";
+import movieReducer from "../features/movies/moviesSlice";
+export default configureStore({
+  reducer: {
+    user: userReducer,
+    movie: movieReducer,
+  },
+  middleware: getDefaultMiddleware({ serializableCheck: false }),
+});
